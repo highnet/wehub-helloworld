6f9042b2057bbd57bbb86c5bd82634001806e5b1
Joaquin <joaquinteleria@gmail.com>
2023-01-27 13:42:39 +0100

    added front-back end communication

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import BaseExtension from "wehub";
+import BaseExtension, { SocketIoSocket } from "wehub";
 
 export default class Helloworld extends BaseExtension {
   static DEFAULT_OPTIONS = {
     option: "value",
   };
+  static NAMESPACE = "helloworld";
   activate() {
     console.log("[Helloworld]: Activated", this.extensionName);
     console.log("[Helloworld]: Option is", this.options["option"]);
     if (MODE.isDevelopment()) {
       console.log("[Helloworld]: I'm in development mode");
     }
+    this.registerMethod(this.logMessage);
+  }
+  logMessage(message: any, socket: SocketIoSocket) {
+    console.log("Received message: " + message);
   }
 }
