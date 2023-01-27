33c362cab1b216dc18bdb8c6a1c49e2a8ed5b547
Joaquin <joaquinteleria@gmail.com>
2023-01-27 12:34:07 +0100

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import BaseExtension from "wehub";
 
 export default class Helloworld extends BaseExtension {
-    activate() {
-        console.log("Activated ", this.extensionName);
+  static DEFAULT_OPTIONS = {
+    option: "value",
+  };
+  activate() {
+    console.log("[Helloworld]: Activated", this.extensionName);
+    console.log("[Helloworld]: Option is", this.options["option"]);
+    if (MODE.isDevelopment()) {
+      console.log("[Helloworld]: I'm in development mode");
     }
+  }
 }
